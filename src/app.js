36876db309e79faa88029411ceb91a79dec7de10
bjorneboman <gostabohman@gmail.com>
2026-04-08 13:20:59 +0200

    Save point #1

--- src/app.js
+++ src/app.js
@@ -2,8 +2,37 @@ const express = require("express")
 
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Server alive 'n' kickin'!")
+
+app.use(express.json())
+
+let users = [
+  {id: 1, name: "Alice", email: "alice@mail.com"},
+  {id: 2, name: "Bob", email: "bob@mail.com"},
+  {id: 3, name: "Charlie", "email": "charlie@mail.com"}
+]
+
+app.get("/users", (req, res) => {
+  res.json(users)
 })
 
-module.exports = app
+app.get/"users/:id", (req, res) => {
+  // const id = parseInt(req.params.id)
+  const user = users.find(u => u.id === parseInt(req.params.id))
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found", error: "user_not_found"
+    })
+  }
+  res.json(user)
+}
+
+app.post("/users", (req, res) => {
+  const newUser = {id: users.length + 1, ...req.body}
+  users.push(newUser)
+  res.status(201).json(newUser)
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
+})
+// module.exports = app
